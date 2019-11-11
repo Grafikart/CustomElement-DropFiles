c5ae1d7c8618f40c00ddd8eadb0cf5e305e75559
Jonthan Boyer <contact@grafikart.fr>
2019-11-11 03:00:55 +0100

    style: git hooks

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   verbose: true,
-  preset: "jest-puppeteer",
+  preset: 'jest-puppeteer',
   moduleFileExtensions: ['js', 'jsx', 'ts'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|scss|less)$': 'identity-obj-proxy',
   },
   coverageThreshold: {
@@ -11,16 +12,13 @@ module.exports = {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100
-    }
+      statements: 100,
+    },
   },
   // modulePathIgnorePatterns: ["<rootDir>/tests/e2e/"],
-  testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
-    "^.+\\.(ts|tsx)?$": "ts-jest"
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
   },
-  setupFilesAfterEnv: ['./tests/setup.js']
+  setupFilesAfterEnv: ['./tests/setup.js'],
 }
